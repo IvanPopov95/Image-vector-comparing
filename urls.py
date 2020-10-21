9e55e550672f2919afefb0296791772b4c33f87b
Popov Ivan <60357215+IvanPopov95@users.noreply.github.com>
2020-10-21 18:02:33 +0300

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import PersonView, PersonDetailView, CompareView
-
-urlpatterns = [
-    path('persons/', PersonView.as_view()),
-    path('persons/compare/', CompareView.as_view()),
-    path('persons/<uuid:person_id>/', PersonDetailView.as_view()),
-]
